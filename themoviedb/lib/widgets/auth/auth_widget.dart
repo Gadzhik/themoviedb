bb4ce6e1bae9b3b288a831f2978d2b00304a2f39
Gadzhik <gkurban@outlook.com>
2022-03-20 20:35:23 +0100

    first commit

---
+++ themoviedb/lib/widgets/auth/auth_widget.dart
@@ -0,0 +1,185 @@
+import 'package:flutter/material.dart';
+import 'package:themoviedb/Theme/app_button_style.dart';
+
+class AuthWidget extends StatefulWidget {
+  const AuthWidget({Key? key}) : super(key: key);
+
+  @override
+  State<AuthWidget> createState() => _AuthWidgetState();
+}
+
+class _AuthWidgetState extends State<AuthWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login to your account'),
+      ),
+      body: ListView(
+        children: [
+          _HeaderWidget(),
+        ],
+      ),
+    );
+  }
+}
+
+class _HeaderWidget extends StatelessWidget {
+  const _HeaderWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final textStyle = const TextStyle(
+      fontSize: 16,
+      color: Colors.black,
+    );
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(height: 25),
+          _FormWidget(),
+          SizedBox(height: 25),
+          Text(
+            'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting',
+            style: textStyle,
+          ),
+          SizedBox(height: 5),
+          // Add Register Button
+          TextButton(
+              style: AppButtonStyle.linkButton,
+              onPressed: () {},
+              child: Text('Register')),
+          SizedBox(height: 25),
+          Text(
+            'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here, content here making it look like readable English.',
+            style: textStyle,
+          ),
+          SizedBox(height: 5),
+          TextButton(
+            style: AppButtonStyle.linkButton,
+            onPressed: () {},
+            child: Text('Verify e-mail'),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _FormWidget extends StatefulWidget {
+  const _FormWidget({Key? key}) : super(key: key);
+
+  @override
+  State<_FormWidget> createState() => __FormWidgetState();
+}
+
+class __FormWidgetState extends State<_FormWidget> {
+  // Autorization
+  final _loginTextController = TextEditingController();
+  final _passwordTextController = TextEditingController();
+
+  // Show error message, under password field
+  String? errorText = null;
+
+  void _auth() {
+    final login = _loginTextController.text;
+    final password = _passwordTextController.text;
+
+    // Валидируем поля
+    if (login == 'admin' && password == 'admin') {
+      errorText = null;
+      print('Open app');
+    } else {
+      errorText = 'Enter valid login or password!';
+    }
+    setState(() {});
+  }
+
+  // Reset password
+  void _resetPassword() {
+    print('reset passwd');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final textStyle = const TextStyle(
+      fontSize: 16,
+      color: Color(0xFF212529),
+    );
+    final color = const Color(0xFF01B4E4);
+    final textFieldDecorator = const InputDecoration(
+      border: OutlineInputBorder(),
+      contentPadding: EdgeInsets.symmetric(horizontal: 10, vertical: 15),
+      isCollapsed: true,
+    );
+    final errorText = this.errorText;
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        if (errorText != null) ...[
+          Text(
+            errorText,
+            style: TextStyle(
+              fontSize: 17,
+              color: Colors.red,
+            ),
+          ),
+          SizedBox(height: 15),
+        ],
+        Text(
+          'Username',
+          style: textStyle,
+        ),
+        SizedBox(height: 5),
+        TextField(
+          controller: _loginTextController,
+          decoration: textFieldDecorator,
+        ),
+        SizedBox(height: 20),
+        Text(
+          'Password',
+          style: textStyle,
+        ),
+        SizedBox(height: 5),
+        TextField(
+          controller: _passwordTextController,
+          decoration: textFieldDecorator,
+          // hide password simbols
+          obscureText: true,
+        ),
+        SizedBox(height: 25),
+        Row(
+          children: [
+            ElevatedButton(
+              onPressed: _auth,
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(color),
+                foregroundColor: MaterialStateProperty.all(Colors.white),
+                textStyle: MaterialStateProperty.all(
+                  TextStyle(fontSize: 16, fontWeight: FontWeight.w700),
+                ),
+                padding: MaterialStateProperty.all(
+                  EdgeInsets.symmetric(
+                    horizontal: 15,
+                    vertical: 8,
+                  ),
+                ),
+              ),
+              child: Text('Login'),
+            ),
+            SizedBox(width: 30),
+            TextButton(
+              onPressed: _resetPassword,
+              style: AppButtonStyle.linkButton,
+              child: Text('Rest password'),
+            ),
+          ],
+        )
+      ],
+    );
+  }
+}
+
+// time 31.20
